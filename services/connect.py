4a489c47026d8b104f2157408ec16c6161224878
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-05-03 15:22:29 +0000

    Email

--- services/connect.py
+++ services/connect.py
@@ -4,6 +4,9 @@ import pandas as pd
 from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 scope = ['https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive'] 
@@ -15,6 +18,40 @@ drive_service = build('drive', 'v3', credentials=credentials)
 base_insucessos = client.open_by_key('1SjqHE5LsPYCCldD7ZoXMKFrRGE1oD9FdhK7uHZ8S8Co').worksheet('Insucessos')
 base_cad = client.open_by_key('1SjqHE5LsPYCCldD7ZoXMKFrRGE1oD9FdhK7uHZ8S8Co').worksheet('CAD')
 base_devolucoes = client.open_by_key('1eqYyWwshEQPo0DpkhdgrG2ZLycJrj8kGprgRoHrnAGc').worksheet('Comprovantes')
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'andrew.dantas@magazineluiza.com.br'
+smtp_password = 'ffbu aqdo duxw dhlu'
+
+
+def enviar_email(texto):
+        # Construir o e-mail
+    sender_email = 'andrew.dantas@magazineluiza.com.br'
+    receiver_email = 'gerente@magazineluiza.com.br'
+    subject = 'Teste de E-mail'
+    body = texto
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Iniciar conexão SMTP
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+
+    # Login no servidor SMTP
+    server.login(smtp_username, smtp_password)
+
+    # Enviar e-mail
+    text = msg.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+
+    # Encerrar conexão SMTP
+    server.quit()
 
 def verificar_pedidos(usuario):
     pedidos = base_insucessos.get_values('a2:n')   
