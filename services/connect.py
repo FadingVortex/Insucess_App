d7fcbb6b766bd57eb85b68af382458cb0e136193
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-05-13 18:53:59 +0000

    Update

--- services/connect.py
+++ services/connect.py
@@ -20,6 +20,9 @@ base_devolucoes = client.open_by_key('1eqYyWwshEQPo0DpkhdgrG2ZLycJrj8kGprgRoHrnA
 
 def inserir_pedido(pedido, transportadora, motivo, observacao, filial):
     try:
+        pedidos = base_insucessos.get_values('a1:a')
+        if str(pedido) in pedidos:
+            return 'Pedido já cadastrado'
         registro = datetime.now() - timedelta(hours=3)
         registro = registro.strftime('%d/%m/%Y %H:%M:%S')
         
