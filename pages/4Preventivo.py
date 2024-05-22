3300373b7ecf25354803c5782a78ea95850dd0e8
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-05-22 18:58:20 +0000

    update

--- pages/4Preventivo.py
+++ pages/4Preventivo.py
@@ -8,6 +8,11 @@ except:
     st.switch_page('./Login.py')
 
 
+c1, c2 = st.columns(2)
+
+c1.page_link('./pages/HomePage.py', label='Home')
+c2.page_link('./pages/3Registro_devolucao.py',label='Registrar Devolução')
+
 st.title('Pedidos Pendentes')
 
 df = preventivo(st.session_state['Usuário'])
