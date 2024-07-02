c2a7e37f463156cb04948629a6e0e91aa6d6cc45
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-07-02 14:21:20 -0300

    Create 7Atualizar_Status.py

---
+++ pages/7Atualizar_Status.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import services.connect as C
+
+st.set_page_config(
+    page_title="Status dos Pedidos",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",  # Pode ser "wide" ou "centered"
+    initial_sidebar_state="collapsed",  # Pode ser "auto", "expanded", ou "collapsed"
+)
+
+if 'df' not in st.session_state or st.session_state['df'] == None:
+    st.session_state['df'] = pd.DataFrame()
+
+st.title('Atualizar Status Pedido')
+
+table = st.empty()
+
+with st.form("FormStatus"):
+
+    Ped = st.number_input('Pedido', 900000000, step=1)
+    status = st.selectbox('Status', ['Entregue', 'Endereço não Localizado', 'Cliente Ausente', 'Avaria'])
+    button = st.form_submit_button('Consultar')
+
+if button:
+    DF = C.consultar_pedidos_status(Ped)
+    st.session_state['df'] = DF
+    df2 = DF.assign(hack='').set_index('hack')
+    table.table(df2)
+
+import_button = st.button("Registrar Status")
+
+if import_button and table.empty:
+    st.write(C.atualizar_status(st.session_state['df'], status, 'teste'))
+    st.session_state['df'] = None
